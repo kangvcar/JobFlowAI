cee0332782b66dc9a839609930f807607849483f
kangvcar <kangvcar@126.com>
2023-03-04 02:58:02 +0800

    修改错别字

--- src/config.mjs
+++ src/config.mjs
@@ -1,7 +1,7 @@
 import defaultImage from './assets/images/default.png';
 
 const CONFIG = {
-  name: 'JobFlowAI',
+  name: 'JobFlow AI',
 
   origin: 'https://astrowind.vercel.app',
   basePathname: '/',
